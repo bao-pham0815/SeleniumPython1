c6dd5931e61df4b8f8739bdf1f5b4a549d6d4cc0
bao-pham0815 <141957035+bao-pham0815@users.noreply.github.com>
2023-08-10 16:00:12 -0500

    Update test_e2e.py

--- PythonSelfFramework/tests/test_e2e.py
+++ PythonSelfFramework/tests/test_e2e.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-# Running a test invoking a browser in background and
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
@@ -16,15 +15,11 @@ class TestOne(BaseClass):
 
         log = self.getLogger()
         homePage = HomePage(self.driver)
-        #self.driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
         homePage.shopItems().click()
         checkOutPage = CheckOutPage(self.driver)
-
         log.info("getting all the card titles")
-
-        #products = self.driver.find_elements(By.XPATH, "//div[@class='card h-100']")
-
         cards = checkOutPage.getCardTitles()
+        
         i = -1
         for card in cards:
             i = i + 1
@@ -33,17 +28,11 @@ class TestOne(BaseClass):
             log.info(cardText)
             if cardText == "Blackberry":
                 checkOutPage.getCardFooter()[i].click()
-                #self.driver.find_element_by_css_selector(".card-footer button")[i].click()
-
-        #self.driver.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
         checkOutPage.checkOutItems().click()
-
 
         self.driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
         self.driver.find_element(By.ID, "country").send_keys("ind")
         log.info("Entering country name as ind")
-        #wait = WebDriverWait(self.driver, 10)
-        #wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, "India")))
         self.verifyLinkPresence("India")
         self.driver.find_element(By.LINK_TEXT, "India").click()
         self.driver.find_element(By.XPATH, "//div[@class='checkbox checkbox-primary']").click()
