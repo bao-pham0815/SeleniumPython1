f02efa73903f8abca1ddc3957907d8dba6de92f3
bao-pham0815 <141957035+bao-pham0815@users.noreply.github.com>
2023-08-10 15:58:49 -0500

    Update test_HomePage.py

--- PythonSelfFramework/tests/test_HomePage.py
+++ PythonSelfFramework/tests/test_HomePage.py
@@ -14,16 +14,7 @@ class TestHomePage(BaseClass):
 
     def test_formSubmission(self,getData):
 
-        # driver.find_element_by_name("name").send_keys("Rahul")
-        #driver.find_element_by_css_selector("input[name='name']").send_keys("Rahul")
-        #driver.find_element_by_id("exampleCheck1").click()
-        #driver.find_element_by_name("email").send_keys("Shetty")
-        # select class provide the methods to handle the options in dropdown
-        #dropdown = Select(driver.find_element_by_id("exampleFormControlSelect1"))
-        #dropdown.select_by_visible_text("Female")
-        #sel.select_by_visible_text("Male")
-        #dropdown.select_by_index(0)
-        #driver.find_element_by_xpath("//input[@type='submit']").click()
+
         log = self.getLogger()
         homepage = HomePage (self.driver)
         log.info("first name is "+getData["firstname"])
@@ -35,13 +26,11 @@ class TestHomePage(BaseClass):
         self.selectOptionByText(homepage.getGender(), getData["gender"])
         homepage.submitForm().click()
 
-        #alert = driver.find_element_by_class_name("alert-success").text
         alertText = homepage.getSuccessMessage().text
 
         assert ("Success" in alertText)
         self.driver.refresh()
 
-#("Bao Pham", "baopham@udemy111.com", "Male"),("Tram Truong", "Truong.Tram@whateverkjd.com", "Female")
     @pytest.fixture(params=HomePageData2.getAllTestData2())
     #@pytest.fixture(params=HomePageData.getAllTestData("Testcase1")) # use this to run only single test case.
     def getData(self, request):
